79f869eac41fe35b9e89805a73b4b082b2c62ab9
Anastasia Barshina <barshina.av@gmail.com>
2023-09-22 00:43:09 +0300

    Add auth decorators

--- acme_project/birthday/models.py
+++ acme_project/birthday/models.py
@@ -1,7 +1,11 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 from django.urls import reverse
 
 from birthday.validators import real_age
+
+
+User = get_user_model()
 
 
 class Birthday(models.Model):
@@ -17,6 +21,9 @@ class Birthday(models.Model):
         'Фото',
         upload_to='birthdays_images',
         blank=True,
+    )
+    author = models.ForeignKey(
+        User, verbose_name='Автор записи', on_delete=models.CASCADE, null=True,
     )
 
     class Meta:
--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,3 +1,7 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http.response import HttpResponse
+from django.shortcuts import get_object_or_404
 from django.views.generic import (
     CreateView, DeleteView, DetailView, ListView, UpdateView
 )
@@ -8,25 +12,42 @@ from birthday.models import Birthday
 from birthday.utils import calculate_birthday_countdown
 
 
+@login_required
+def simple_view(request):
+    return HttpResponse('Страница для залогиненных пользователей!')
+
+
 class BirthdayListView(ListView):
     model = Birthday
     ordering = 'id'
     paginate_by = 8
 
 
-class BirthdayCreateView(CreateView):
+class BirthdayCreateView(LoginRequiredMixin, CreateView):
     model = Birthday
     form_class = BirthdayForm
 
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
-class BirthdayUpdateView(UpdateView):
+
+class BirthdayUpdateView(LoginRequiredMixin, UpdateView):
     model = Birthday
     form_class = BirthdayForm
 
+    def dispatch(self, request, *args, **kwargs):
+        get_object_or_404(Birthday, pk=kwargs['pk'], author=request.user)
+        return super().dispatch(request, *args, **kwargs)
 
-class BirthdayDeleteView(DeleteView):
+
+class BirthdayDeleteView(LoginRequiredMixin, DeleteView):
     model = Birthday
     success_url = reverse_lazy('birthday:list')
+
+    def dispatch(self, request, *args, **kwargs):
+        get_object_or_404(Birthday, pk=kwargs['pk'], author=request.user)
+        return super().dispatch(request, *args, **kwargs)
 
 
 class BirthdayDetailView(DetailView):
@@ -35,6 +56,6 @@ class BirthdayDetailView(DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['birthday_countdown'] = calculate_birthday_countdown(
-            self.object.birthday
+            self.object.birthday  # type: ignore
         )
         return context
