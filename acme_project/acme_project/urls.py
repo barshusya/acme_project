77d2c4c5545b3987b5db10fd830224ffb849925d
Anastasia Barshina <barshina.av@gmail.com>
2023-09-21 11:46:38 +0300

    Fix registration form

--- acme_project/acme_project/urls.py
+++ acme_project/acme_project/urls.py
@@ -1,9 +1,11 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.contrib.auth.forms import UserCreationForm
 from django.urls import include, path, reverse_lazy
 from django.views.generic.edit import CreateView
+
+from users.forms import CustomUserCreationForm
+
 
 urlpatterns = [
     path('', include('pages.urls')),
@@ -12,7 +14,7 @@ urlpatterns = [
         'auth/registration/',
         CreateView.as_view(
             template_name='registration/registration_form.html',
-            form_class=UserCreationForm,
+            form_class=CustomUserCreationForm,
             success_url=reverse_lazy('pages:homepage'),
         ),
         name='registration',
